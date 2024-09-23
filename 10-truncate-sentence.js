d57f02bb4b39ee7c4c9ec5b3e4573832b0d79b0d
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-23 11:40:41 +0530

    Done

--- 10-truncate-sentence.js
+++ 10-truncate-sentence.js
@@ -1,43 +1,21 @@
-//truncate Sentence Lab
-//TODO implement without using split
+//Truncate Sentence Lab
+// Using split function to split a sentence into array then perform the task on that array.
+//......................................................
+
+function truncateSentence(sent, count) {
+    let words = sent.split(' ');
+    words.length=count;
 
 
-// function truncateSentence(sent, count) {
-//     let words = sent.split(' ');
-//     words.length=count;
-
-
-//     //let truncatedWords = [];
+    let truncatedWords = [];
   
-//     // for (let i = 0; i <count; i++) {
-//     //     let present = words[i];
-//     //     truncatedWords.push(present);
+    for (let i = 0; i <count; i++) {
+        let present = words[i];
+        truncatedWords.push(present);
       
-//     // }
-//     return words.join(' ');
-//   }
+    }
+    return words.join(' ');
+  }
 
-//   console.log(truncateSentence("What is the solution to this problem",6)); 
-
-
-function truncateSentence(Sentence,counter){
-    let count =0;
-    let len=Sentence.length;
-    let array = new Array(len)
-    for(let i=0; i<len; i++){
-        if(Sentence[i] ===' '){
-            count++;
-        }
-        if(count!==counter){
-            array[i]=Sentence[i];
-        }else if(count>counter){
-            break;
-        }
-    } 
-    return array.toString();
-
-
-}
-  
+    
 console.log(truncateSentence("What is the solution to this problem",6)); 
- // "What is the solution"
