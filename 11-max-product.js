dea8e988bef03fff832545ce17d700c7ca4c3e69
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-23 11:42:43 +0530

    Done

--- 11-max-product.js
+++ 11-max-product.js
@@ -1,24 +1,26 @@
 //Maximum Product of Three Numbers in array
-// TODO implement without using sort function and loops over  
-// function maximumProduct(nums) {
-   
-//     nums.sort()
-
-//    let product =1;
-//     let count=0;
-//     let arrlength=nums.length-1;
-//         while(count<3){
-//            product*=nums[arrlength]
-//            arrlength--
-//             count++
-//        }
-//        return product;
-//     }
-
-// console.log(maximumProduct([1,2,3,4,8,5,9,5,7,2]))
-//Maximum Product of Three Numbers
+// TODO implement without using sort function and loops over 
+// Sorted the array in ascending order Then calculated the product of the maximum three numbers
 
 function maximumProduct(nums) {
+   
+    nums.sort()
+
+   let product =1;
+    let count=0;
+    let arrlength=nums.length-1;
+        while(count<3){
+           product*=nums[arrlength]
+           arrlength--
+            count++
+       }
+       return product;
+    }
+
+console.log(maximumProduct([1,2,3,4,8,5,9,5,7,2]))
+
+
+
 
     
 
